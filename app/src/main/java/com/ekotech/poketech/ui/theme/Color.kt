997c579c937d87a61f0c7a61568aea59ece76f64
Kunle Ogunjimi <kdotkid3@gmail.com>
2024-01-11 08:28:59 +0000

    status bar now app red color

--- app/src/main/java/com/ekotech/poketech/ui/theme/Color.kt
+++ app/src/main/java/com/ekotech/poketech/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val appRed = Color(0xFFB71C1C)
+val appRedLight = Color(0xFFfc506a)
+val appTeal = Color(0xFF1CB7B7)
